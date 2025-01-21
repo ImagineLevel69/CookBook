04dc99a296810b1e1dd06b2d8e6f06e5089b8099
ImagineLevel69 <tgtullett@gmail.com>
2025-01-21 19:00:54 +0000

    Search bar working. Maybe

---
+++ search.js
@@ -0,0 +1,5 @@
+document.getElementById('search-btn').addEventListener('click', function () {
+    var searchContainer = document.querySelector('.search-container');
+    searchContainer.classList.toggle('active');
+});
+
